ad99ddd01432105bbf83bd79cc56eed6d6978430
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-07 02:38:19 +0500

    backend pushed

---
+++ middleware/AuthMiddleware.js
@@ -0,0 +1,29 @@
+const asyncHandler = require("express-async-handler");
+const userModel = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const ErrorHandler = require("../utils/ErrorHandler");
+
+const isAuthenticatedUser = asyncHandler(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    next(new ErrorHandler("please login to access this resource", 401));
+  }
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await userModel.findById(decodedData.id);
+  next();
+});
+const authRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resouce `,
+          403
+        )
+      );
+    }
+
+    next();
+  };
+};
+module.exports = { isAuthenticatedUser, authRoles };
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+
+const cloudinary = require("cloudinary");
+const cookieParser = require("cookie-parser");
+const trainRoutes = require("./routes/trainRoutes");
+const userRoutes = require("./routes/userRoutes");
+const ticketroutes = require("./routes/ticketRoutes");
+
+const connectDB = require("./database");
+const bodyparser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const errHandler = require("./middleware/error");
+
+require("dotenv").config();
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.use(fileUpload());
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
+
+connectDB();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.use("/api/v1", trainRoutes);
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", ticketroutes);
+app.use(errHandler);
+app.listen(process.env.PORT, () => {
+  console.log(`server is listening at port:${process.env.PORT}`);
+});
