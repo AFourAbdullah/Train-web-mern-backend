1592563935087a1c1dff2203ca1e05b452c4a7e0
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-07 19:15:32 +0500

    changes added

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ require("dotenv").config();
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 app.use(fileUpload());
+app.set("trust proxy", 1);
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
