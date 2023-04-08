2c4dd22c0da154f90e6fb8360ce7ee7ef8d3dc4b
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-08 19:17:54 +0500

    backend finalized

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ const errHandler = require("./middleware/error");
 
 require("dotenv").config();
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["POST", "PUT", "GET", "DELETE", "HEAD"],
+    credentials: true,
+  })
+);
 app.use(fileUpload());
 app.set("trust proxy", 1);
 
