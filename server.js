bca43a209222194a0644d8c856805c3c5785133c
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-09 04:15:48 +0500

    backend finalized

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 // app.use(cors());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://trainistanapp.netlify.app/",
     methods: ["GET", "PUT", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
     credentials: true,
