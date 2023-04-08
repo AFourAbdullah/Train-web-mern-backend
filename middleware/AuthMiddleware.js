bbe90a085b7b48b6461357120c5ed0c4a1cbbf4e
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-08 19:10:33 +0500

    backend addded

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -4,13 +4,7 @@ const jwt = require("jsonwebtoken");
 const ErrorHandler = require("../utils/ErrorHandler");
 
 const isAuthenticatedUser = asyncHandler(async (req, res, next) => {
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  }
+  const { token } = req.cookies;
   if (!token) {
     next(new ErrorHandler("please login to access this resource", 401));
   }
