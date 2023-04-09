c80e4dc3c435fc6cc08eeac8598a6e401e8dbcee
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-09 06:32:01 +0500

    backend addded

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "50mb" }));
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://192.168.100.29:3000",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
