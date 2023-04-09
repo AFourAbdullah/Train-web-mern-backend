8450b96fbac544d9b5b8891deb01f6d59b08883e
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-09 21:01:52 +0500

    backend new added

--- utils/sendingJWTtoken.js
+++ utils/sendingJWTtoken.js
@@ -7,7 +7,7 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    // sameSite: "strict", // set SameSite attribute to "None"
+    sameSite: "None", // set SameSite attribute to "None"
     secure: true, // use Secure attribute for HTTPS connections
   };
   res.status(statusCode).cookie("token", token, options).json({
