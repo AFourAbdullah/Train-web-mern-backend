3c838ff2c889d1f6ea08891031b8e7b89c556f51
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-10 00:57:18 +0500

    backend new added

--- server.js
+++ server.js
@@ -22,13 +22,7 @@ const errHandler = require("./middleware/error");
 require("dotenv").config();
 app.use(express.json({ limit: "50mb" }));
 // app.use(cors());
-app.use(
-  cors({
-    origin: "https://fastidious-kelpie-e5e6b0.netlify.app",
-
-    // , credentials: true
-  })
-);
+app.use(cors());
 app.use(fileUpload());
 app.set("trust proxy", 1);
 // app.use(
