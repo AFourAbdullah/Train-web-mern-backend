db4158f3673deb7abf991507eddc9859ba102065
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-09 23:40:45 +0500

    backend new added

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "50mb" }));
 // app.use(cors());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://fastidious-kelpie-e5e6b0.netlify.app",
 
     // , credentials: true
   })
