a6c73c9a0701fee97825eb295e9321559a1df15c
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-10 01:05:35 +0500

    backend new added

--- server.js
+++ server.js
@@ -22,11 +22,20 @@ const errHandler = require("./middleware/error");
 require("dotenv").config();
 app.use(express.json({ limit: "50mb" }));
 // app.use(cors());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+var cors = require("cors");
+const corsOptions = {
+  // origin:'*',
+  origin: true,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(fileUpload());
 app.set("trust proxy", 1);
 // app.use(
