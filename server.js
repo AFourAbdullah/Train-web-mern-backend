6be7c02ef3d3d5facf974dbdc53117e8d58bd884
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-09 20:56:37 +0500

    backend new added

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const cookieParser = require("cookie-parser");
 const trainRoutes = require("./routes/trainRoutes");
 const userRoutes = require("./routes/userRoutes");
 const ticketroutes = require("./routes/ticketRoutes");
+const allowedOrigins = ["http://localhost:3000"];
 
 const connectDB = require("./database");
 const bodyparser = require("body-parser");
@@ -20,7 +21,13 @@ app.use(express.json({ limit: "50mb" }));
 // app.use(cors());
 app.use(
   cors({
-    origin: "*",
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
 app.use(fileUpload());
