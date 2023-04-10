499bae6df020063a750ddcc013050b1842b9c58a
Abdullah Aziz <abdullahazees4@gmail.com>
2023-04-10 22:46:04 +0500

    backend finalized

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json({ limit: "50mb" }));
 var cors = require("cors");
 const corsOptions = {
   // origin:'*',
-  origin: true,
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
